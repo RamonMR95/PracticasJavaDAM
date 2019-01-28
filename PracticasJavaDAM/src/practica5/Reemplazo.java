f7d15666f18a57e2d3125f8ffc47159b51b917cf
Ramón Moñino <ramonmr16@gmail.com>
2019-01-28 20:43:14 +0100

    Update Practicas

--- PracticasJavaDAM/src/practica5/Reemplazo.java
+++ PracticasJavaDAM/src/practica5/Reemplazo.java
@@ -1,10 +1,11 @@
 package practica5;
-
+//TODO
 public class Reemplazo {
 
 	public static void main(String[] args) {
 		String texto = "Oracle ha anunciado hoy su nueva generación de compilador Java hoy. Utiliza analizador avanzado y optimizador especial para la JVM de Oracle";
 		String claves = "JVM,Java,Oracle";
+		String [] arrayClaves= claves.split(",");	
 		System.out.println(reemplazaPalabras(texto, claves));
 	}
 	
