26f6597a22a1d1a51dd0136556b59cb1f5b264bd
Ramón Moñino <ramonmr16@gmail.com>
2019-03-01 19:41:41 +0100

    Fix Javadoc Autoev6

--- PracticasJavaDAM/src/autoevaluacion6/ListaLink.java
+++ PracticasJavaDAM/src/autoevaluacion6/ListaLink.java
@@ -3,6 +3,7 @@ package autoevaluacion6;
 /**
  * Representa la implementación parcial de una lista enlazada simple en la que
  * se tiene acceso directo al primer elemento y también al último.
+ * @author Ramón Moñino Rubio
  */
 public class ListaLink {
 
@@ -40,22 +41,31 @@ public class ListaLink {
 		numElementos = 0;
 	}
 
+	/**
+	 * Metodo que añade en nodos a una lista enlazada dado un índice
+	 * @param indice - En el que queremos insertar
+	 * @param dato - Dato a insertar en la lista
+	 * @author Ramon Moñino Rubio
+	 */
 	public void add(int indice, Object dato) {
 		assert dato != null;
 
-		Nodo nuevo = new Nodo();
+		Nodo nuevo = new Nodo();	// Creación del nuevo nodo a insertar
 		nuevo.dato = dato;
 		Nodo actual = primero;
 
+		/* Si la lista está vacia se inserta en la primera posición */
 		if (numElementos == 0) {
 			primero = nuevo;
 			ultimo = nuevo;
 			
+		/* Inserción por el final */
 		} else if (indice == numElementos && numElementos != 0) {
 			ultimo.siguiente = nuevo;
 			nuevo.siguiente = null;
 			ultimo = nuevo;
 			
+		/* Recorremos la lista hasta la posición anterior a indice e insertamos el nuevo nodo */
 		} else {
 			for (int i = 0; i < indice - 1; i++) {
 				actual = actual.siguiente;
@@ -68,6 +78,9 @@ public class ListaLink {
 
 	}
 
+	/**
+	 * Metodo auxiliar que visualiza la lista enlazada
+	 */
 	public void visualizar() {
 		Nodo actual = primero;
 
--- PracticasJavaDAM/src/autoevaluacion6/Main.java
+++ PracticasJavaDAM/src/autoevaluacion6/Main.java
@@ -1,12 +1,23 @@
 package autoevaluacion6;
 
+/**
+ * Metodo que prueba la clase ListaLink
+ * @author ramon
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
+		/* Creamos un objeto lista */ 
 		ListaLink lista = new ListaLink();
+		
+		/* Insertamos en diversas posiciones */
 		lista.add(0, 1);
+		lista.add(1, 4);
 		lista.add(1, 2);
 		lista.add(2, 3);
+		
+		/* Visualizamos la lista */
 		lista.visualizar();
 	}
 
